c0154c72237998d5e33d7ffa3bb36bebe625da14
NaoKanazawa <naomiti3333@gmail.com>
2019-12-16 02:02:33 +0900

    update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
+import { MapService } from '../services/map.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+  value = '';
+  results: google.maps.places.PlaceResult[];
+  constructor(private mapService: MapService) {}
 
-  constructor() {}
+  search() {
+    this.mapService
+      .geocode({ address: this.value })
+      .then(result => {
+        this.map.center = result.geometry.location;
+        this.nearbySearch(result.geometry.location);
+      })
+      .catch(() => {
+        console.log('geocode 失敗');
+      });
+  }
 
+  private nearbySearch(latLng: google.maps.LatLng): void {
+    const placeService = new google.maps.places.PlacesService(
+      this.map.data.getMap()
+    );
+    const request: google.maps.places.PlaceSearchRequest = {
+      location: latLng,
+      radius: 50000,
+      keyword: `${this.value} コーヒー`
+    };
+    this.mapService
+      .nearbySearch(placeService, request)
+      .then(results => {
+        this.results = results;
+      })
+      .catch(() => {
+        console.log('nearbySearch 失敗');
+      });
+  }
 }
