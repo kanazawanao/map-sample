1d2abc3ef80cc53b265b379877da86e3247fd17c
NaoKanazawa <naomiti3333@gmail.com>
2019-12-15 22:54:19 +0900

    update

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    GoogleMapsModule
   ],
   declarations: [Tab1Page]
 })
