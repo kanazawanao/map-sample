ab41b5793696afca6a1804ffa0e121d5161d1e55
NaoKanazawa <naomiti3333@gmail.com>
2019-12-15 23:11:39 +0900

    geocode実装

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
+import { MapService } from '../services/map.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+  value = '';
+  constructor(private mapService: MapService) {}
 
-  constructor() {}
-
+  geocode() {
+    this.mapService
+      .geocode({ address: this.value })
+      .then(result => {
+        this.map.center = result.geometry.location;
+      })
+      .catch(() => {
+        console.log('geocode 失敗');
+      });
+  }
 }
